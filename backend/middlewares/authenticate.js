656e2fdfe4257f7eb1a608eb1f6c18ec5e74cb73
shakti sumant das <shakti.20k@gmail.com>
2024-10-16 23:13:41 +0530

    add jwt authetication

---
+++ backend/middlewares/authenticate.js
@@ -0,0 +1,23 @@
+const jwt=require("jsonwebtoken");
+
+const authenticate = async(req,res,next) => {
+     try {
+        const token=req.cookies.token;
+        if(!token){
+            return res.status(401).json({msg:"Authorization token is required"});
+        }
+        
+        // Verify the token 
+        const decode=jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(401).json({msg:"Invalid token"});
+        }
+
+        req.id=decode.id;
+     } catch (error) {
+        console.error("Error during authentication:", error);
+        return res.status(401).json({ msg: "Invalid or expired token" });
+     }
+}
+
+module.exports=authenticate;
