432c8441dab493a570ae80c06cd956bdb91ceafb
shakti sumant das <shakti.20k@gmail.com>
2024-10-20 18:48:40 +0530

    setup layouts for app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,46 @@
+import AuthLayout from "./layout/AuthLayout";
+import MainLayout from "./layout/MainLayout";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <h2>About</h2>,
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   
-
+  
   return (
-    <>
-     Hello world
-    </>
+    <RouterProvider router={router} />
   )
 }
 
