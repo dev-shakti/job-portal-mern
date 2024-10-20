bfa0a207738dbe34458af2ed1a8042934d1cf8aa
shakti sumant das <shakti.20k@gmail.com>
2024-10-20 19:54:19 +0530

    add register  and login page jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,62 @@
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Link } from "react-router-dom"
+
 const LoginPage = () => {
   return (
-    <div>
-      LoginPage
-    </div>
+    <section className="flex justify-center items-center h-screen">
+    <Card className="w-full max-w-sm">
+        <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+                Enter your email below to login to your account. <br />
+            </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                />
+            </div>
+            <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input  id="password" type="password" required />
+            </div>
+        </CardContent>
+        <CardFooter>
+            <div className="w-full">
+                <Button
+                   
+                    className="w-full"
+                  >
+                   
+
+                    <span className="ml-2">Sign in</span>
+                </Button>
+
+                <div className="mt-4 text-center text-sm">
+                    Don't have an account?
+                    <Link to={'/auth/register'} className="underline">
+                        Sign up
+                    </Link>
+                </div>
+            </div>
+        </CardFooter>
+    </Card>
+</section>
   )
 }
 
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,11 +1,101 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <div>
-      RegisterPage
-    </div>
-  )
-}
+    <section className="flex justify-center items-center h-screen">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Signup</CardTitle>
+          <CardDescription>
+            Enter your details below to signup to your account. <br />
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Fullname</Label>
+            <Input
+              id="fullanme"
+              type="text"
+              name="fullanme"
+              placeholder="John Doe"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Phone Number</Label>
+            <Input id="phone" type="text" placeholder="9326856937" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <div className="grid gap-2">
+            <RadioGroup className="flex items-center gap-4">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="option-one">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="option-two">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <div className="grid gap-2">
+            <Label >Profile</Label>
+            <Input type="file" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="w-full">
+            <Button className="w-full">
+              <span className="ml-2">Sign up</span>
+            </Button>
 
-export default RegisterPage
+            <div className="mt-4 text-center text-sm">
+              Already have an account?
+              <Link to={"/auth/login"} className="underline">
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
+
+export default RegisterPage;
