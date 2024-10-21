7f1483bfe066ccaa71605a830c71add41a4a4b5d
shakti sumant das <shakti.20k@gmail.com>
2024-10-21 14:24:58 +0530

    add carousal category component

--- frontend/src/components/CarousalCats.jsx
+++ frontend/src/components/CarousalCats.jsx
@@ -1,9 +1,42 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "./ui/button";
+
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "FullStack Developer",
+];
+
 const CarousalCats = () => {
   return (
-    <div>
-      CarousalCats
+    <div className="my-20">
+      <Carousel className="w-full max-w-xl mx-auto">
+        <CarouselContent className="-ml-1">
+          {category.map((cat, index) => (
+            <CarouselItem
+              key={index}
+              className="pl-1 md:basis-1/2 lg:basis-1/3"
+            >
+              <Button variant="outline" className="rounded-full">
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default CarousalCats
+export default CarousalCats;
