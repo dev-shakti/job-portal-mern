3a4dbcba4aa783a915ebb9642157a48406c978a1
shakti sumant das <shakti.20k@gmail.com>
2024-10-22 22:12:38 +0530

    add env file in frontend

--- frontend/src/http/apis.js
+++ frontend/src/http/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apis = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 export const registerUser = async (data) => {
