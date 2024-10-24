7e059d45a86001c74c2a80d42beb3df37b91340c
shakti sumant das <shakti.20k@gmail.com>
2024-10-24 22:21:23 +0530

    add job slice

---
+++ frontend/src/store/jobSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    allJobs:[],
+    singleJob:null 
+  };
+const jobSlice=createSlice({
+    name:"job",
+    initialState,
+    reducers:{
+        getAllJobs:(state,action) => {
+            state.allJobs=action.payload;
+        },
+        getSingleJob:(state,action)=> {
+            state.singleJob=action.payload;
+        },
+    }
+})
+
+
+export const { getAllJobs,getSingleJob } = jobSlice.actions;
+
+export default jobSlice.reducer;
