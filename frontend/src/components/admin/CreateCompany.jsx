3d3c49ef35a8d9467cb97e8eee4ea8359102e530
shakti sumant das <shakti.20k@gmail.com>
2024-10-25 23:57:41 +0530

    register new company

--- frontend/src/components/admin/CreateCompany.jsx
+++ frontend/src/components/admin/CreateCompany.jsx
@@ -2,11 +2,29 @@ import { useState } from "react";
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import axios from "axios";
+import { COMPANY_API_END_POINT } from "@/utilis/const";
 
 const CreateCompany = () => {
-
-  const [input,setInput]=useState('');
-
+  const [companyName, setCompanyName] = useState("");
+  
+  const registerNewCompany = async () => {
+    try {
+      const response = await axios.post(
+        `${COMPANY_API_END_POINT}/registerCompany`,
+        {companyName},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      console.log(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="max-w-4xl mx-auto my-10">
       <div className="my-10">
@@ -21,13 +39,15 @@ const CreateCompany = () => {
             type="text"
             placeholder="Microsoft, Amazon etc..."
             className="my-2"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
           />
         </div>
         <div>
           <Button variant="outline">Cancel</Button>
-          <Button className="ml-4">Continue</Button>
+          <Button className="ml-4" onClick={registerNewCompany}>
+            Continue
+          </Button>
         </div>
       </div>
     </div>
