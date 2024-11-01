a0c08946ddd51364482b05187969b1f080ce73c7
shakti sumant das <shakti.20k@gmail.com>
2024-11-01 15:08:59 +0530

    navigate job cards to job detail page

--- frontend/src/components/LatestJobCard.jsx
+++ frontend/src/components/LatestJobCard.jsx
@@ -7,32 +7,33 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "./ui/badge";
+import { Link } from "react-router-dom";
 
-const LatestJobCard = ({job}) => {
+const LatestJobCard = ({ job }) => {
   return (
-    <Card className="shadow-lg mb-4 md:mb-0">
-      <CardHeader>
-        <CardTitle>{job?.company?.name}</CardTitle>
-        <CardDescription className=" text-slate-500">India</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p className='font-medium text-lg text-slate-700'>{job?.title}</p>
-        <p className="text-sm text-slate-500">
-        {job?.description}
-        </p>
-      </CardContent>
-      <CardFooter>
-        <Badge className={"text-[#4834d4] font-bold"} variant="ghost">
-        {job?.position} Positions
-        </Badge>
-        <Badge className={"text-[#F83002] font-bold"} variant="ghost">
-        {job?.jobType} 
-        </Badge>
-        <Badge className={"text-[#8e44ad] font-bold"} variant="ghost">
-          {job?.salary} LPA
-        </Badge>
-      </CardFooter>
-    </Card>
+    <Link to={`/jobdetails/${job._id}`}>
+      <Card className="shadow-lg mb-4 md:mb-0">
+        <CardHeader>
+          <CardTitle>{job?.company?.name}</CardTitle>
+          <CardDescription className=" text-slate-500">India</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="font-medium text-lg text-slate-700">{job?.title}</p>
+          <p className="text-sm text-slate-500">{job?.description}</p>
+        </CardContent>
+        <CardFooter>
+          <Badge className={"text-[#4834d4] font-bold"} variant="ghost">
+            {job?.position} Positions
+          </Badge>
+          <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+            {job?.jobType}
+          </Badge>
+          <Badge className={"text-[#8e44ad] font-bold"} variant="ghost">
+            {job?.salary} LPA
+          </Badge>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
