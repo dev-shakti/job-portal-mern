70051df2ad22582f6b15996a5e19c10659417d7b
shakti sumant das <shakti.20k@gmail.com>
2024-11-08 16:36:19 +0530

    set up app for production

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,11 @@ const companyRouter=require("./routes/companyRoutes")
 const jobRouter=require("./routes/jobRoutes")
 const applicationRouter=require("./routes/applicationRoutes")
 const cookieParser = require('cookie-parser');
+const path=require('path')
 
 const PORT = process.env.PORT || 3000;
+const _dirname=path.resolve()
+
 
 //Middlewares
 app.use(express.json());
@@ -27,6 +30,11 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/company", companyRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get("*",(_,res) => {
+  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+})
 
 // Connect to MongoDB and start server
 connectToDb(process.env.MONGO_URI)
